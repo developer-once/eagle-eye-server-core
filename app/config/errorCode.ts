609b77ef65c70fb9d8297d02d71ce74c1083a28b
mriliujy <mriliujy@gmail.com>
2022-09-13 15:38:51 +0800

    fix(errorcode): remove useless error code

--- app/config/errorCode.ts
+++ app/config/errorCode.ts
@@ -14,48 +14,12 @@ export default {
       msg: 'app_key 不合法',
     }
   },
-  102: () => {
-    return {
-      code: 102,
-      data: "",
-      msg: '无权限',
-    }
-  },
-  110: () => {
-    return {
-      code: 110,
-      data: "",
-      msg: '项目已存在，请修改项目名称后重试',
-    }
-  },
   200: (data: any) => {
     return {
       code: 200,
       data,
       msg: 'success',
     };
-  },
-  // ------------------ User ------------------
-  300: () => {
-    return {
-      code: 300,
-      data: "",
-      msg: '请重新登录',
-    }
-  },
-  301: () => {
-    return {
-      code: 301,
-      data: "",
-      msg: '邮箱或密码错误',
-    }
-  },
-  302: () => {
-    return {
-      code: 302,
-      data: "",
-      msg: '用户名或邮箱已存在，请修改后重试',
-    }
   },
   // ------------------ Server ------------------
   500: (data: any) => {
